f6e925a846b32814743a5eade138489e11678070
leonliang33 <llian008@fiu.edu>
2016-04-16 17:26:27 -0400

    try this mudahfuckah

--- Snag/node/geofence.js
+++ Snag/node/geofence.js
@@ -8,8 +8,8 @@ var dbhandler = require("./dbhandler.js");
 
 //test data
 var radius = 2;
-var custLat = -80.1909090;
-var custLong = 25.7776200;
+var custLat = 180.1909090;
+var custLong = 125.7776200;
 var user = 'Fried Twinky';
 getCoordinates(radius, user, custLong, custLat);
 
@@ -17,14 +17,16 @@ function getCoordinates(radius, user, custLong, custLat) {
     var db = MongoClient.connect("mongodb://52.201.9.182:27017/emerge", function(err, db){
         var cursor = db.collection('sales').find(
             { name: user },
-            { long: 1, lat: 1, _id: 0 }
+            { long: 1, lat: 1}
             );
-        if (checkLocation(radius, custLat, cursor.lat, custLong, cursor.long)) {
-            //print("user is in range");
-            console.log("user is in range");
-            //return true;
-        };
-    });
+        
+            if (checkLocation(radius, custLat, cursor.lat, custLong, cursor.long)) {
+                print("user is in range");
+                console.log("user is in range : %s %s", cursor.lat, cursor.long);
+                return true;
+            };
+        });
+    
 }
 
 function createFence(x,y) {
@@ -41,10 +43,10 @@ function createFence(x,y) {
 function checkLocation(radius, lat1, lat2, lon1, lon2) {
     radius = radius * 1.60934;
     var R = 6371000; // metres
-    var l1 = lat1.toRadians();
-    var l2 = lat2.toRadians();
-    var dl = (lat2 - lat1).toRadians();
-    var de = (lon2 - lon1).toRadians();
+    var l1 = lat1 * 0.0174533;
+    var l2 = lat2 * .0174533;
+    var dl = (lat2 - lat1) * 0.0174533;
+    var de = (lon2 - lon1) * 0.0174533;
 
     var a = Math.sin(dl / 2) * Math.sin(dl / 2) +
             Math.cos(l1) * Math.cos(l2) *
