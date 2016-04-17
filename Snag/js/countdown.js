b63df395a2ed86c5b0d1bc3601fc82a34ca1d99a
Ubuntu <ubuntu@ip-172-31-3-150.ec2.internal>
2016-04-17 12:40:55 +0000

    changed countdown timer

--- Snag/js/countdown.js
+++ Snag/js/countdown.js
@@ -38,7 +38,7 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+var deadline = new Date(Date.parse(new Date()) + 2 * 60 * 60 * 1000);
 
 function getDeadline(x) {
   return new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
